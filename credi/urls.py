8dc98fc319aadeda65ee095a11b8dfc19121d28c
Douglas Silva <douglasitpro@gmail.com>
2024-10-08 19:47:25 -0300

    Ajustando no path para core_app.urls

--- credi/urls.py
+++ credi/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', include('core_app.urls')),
 ]
 
 if settings.DEBUG:
