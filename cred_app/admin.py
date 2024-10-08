d5bace5e3d7e78373dd800a5f901f430b3bdff4a
Douglas Silva <douglasitpro@gmail.com>
2024-10-08 13:02:28 -0300

    Adicionando ao formulário admin miniatura como icone da foto do titular

--- cred_app/admin.py
+++ cred_app/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 from .models import Titular
 
+from django.utils.html import format_html
+
 class TitularAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cpf', 'email', 'telefone', 'criado_em', 'usuario')
+    list_display = ('foto_com_icone','nome', 'cpf', 'email', 'telefone', 'criado_em', 'usuario')
     search_fields = ('nome', 'cpf', 'email')
 
     def save_model(self, request, obj, form, change):
         if not change:
             obj.usuario = request.user
         super().save_model(request, obj, form, change)
-        
+
+    #Apresentar a foto como miniatura no admin
+    def foto_com_icone(self, obj):
+        if obj.foto:
+            return format_html(
+                '<img src="{}" style="width: 50px; height: 50px; border-radius: 50%;" /> <span class="dashicons dashicons-format-image" style="font-size: 20px;"></span>',
+                obj.foto.url  # busca a url da foto
+            )
+        return "Sem foto"  # se não conter a foto
+
+    foto_com_icone.short_description = 'Foto'  # descrição da foto no admin
+
+
 admin.site.register(Titular, TitularAdmin)
