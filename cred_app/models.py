c41cbab72ef939d3c40531e275b514f9d4dbc09f
Douglas Silva <douglasitpro@gmail.com>
2024-10-08 13:12:10 -0300

    Criando a Tabela model de convidados

--- cred_app/models.py
+++ cred_app/models.py
@@ -16,3 +16,21 @@ class Titular(models.Model):
  
     def __str__(self):
         return self.nome
+    
+
+class Convidado(models.Model):
+    titular = models.ForeignKey(Titular, on_delete=models.CASCADE, related_name='convidados')
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14, unique=True)
+    email = models.EmailField(unique=True)
+    telefone = models.CharField(max_length=20, blank=True)
+    foto = models.ImageField(upload_to='fotos/convidados/', null=True, blank=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Convidado"
+        verbose_name_plural = "Convidados"
+
+    def __str__(self):
+        return self.nome
