88c29bc9320cc4789b51306fed5cefb2d99cea86
Douglas Silva <douglasitpro@gmail.com>
2024-10-08 13:40:22 -0300

    Adicionando Tabela model Evendos e verboses

--- cred_app/models.py
+++ cred_app/models.py
@@ -34,3 +34,21 @@ class Convidado(models.Model):
 
     def __str__(self):
         return self.nome
+    
+
+class Evento(models.Model):
+    nome = models.CharField(max_length=255, verbose_name="Nome do Evento")
+    data = models.DateTimeField(verbose_name="Data de Início")
+    data_termino = models.DateTimeField(verbose_name="Data de Término", null=True, blank=True)
+    local = models.CharField(max_length=255, verbose_name="Local do Evento")
+    descricao = models.TextField(verbose_name="Descrição do Evento")
+    foto = models.ImageField(upload_to='fotos/eventos/', null=True, blank=True, verbose_name="Foto do Evento")
+    criado_em = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Evento"
+        verbose_name_plural = "Eventos"
+
+    def __str__(self):
+        return self.nome
