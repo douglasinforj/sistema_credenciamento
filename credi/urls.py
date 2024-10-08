9de7e594eab972596cd46e6bac2612553b17794c
Douglas Silva <douglasitpro@gmail.com>
2024-10-08 19:58:36 -0300

    Ajustando no path para cred_app

--- credi/urls.py
+++ credi/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core_app.urls')),
+    path('cred_app', include('cred_app.urls'))
 ]
 
 if settings.DEBUG:
