c91f51b04a897729b7801a5400958594743cdaa1
Douglas Silva <douglasitpro@gmail.com>
2024-10-08 20:02:14 -0300

    criando view listar_titulares, mostrar os dados renderizados

--- cred_app/views.py
+++ cred_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Titular
 
-# Create your views here.
+
+def listar_titulares(request):
+    titulares = Titular.objects.all() 
+    return render(request, 'cred_app/listar_titulares.html', {'titulares': titulares})
