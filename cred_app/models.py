0bb94ccbb018e9536c685cf9023b70b296f00a31
Douglas Silva <douglasitpro@gmail.com>
2024-10-08 12:38:30 -0300

    Tratando com class meta o plural do titulo no admin

--- cred_app/models.py
+++ cred_app/models.py
@@ -10,5 +10,9 @@ class Titular(models.Model):
     criado_em = models.DateTimeField(auto_now_add=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = "Titular"
+        verbose_name_plural = "Titulares"
+ 
     def __str__(self):
         return self.nome
